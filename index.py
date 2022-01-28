98f9bcaa449ac58f50e7ef5457a70fb809bd7162
Szymon <wawrzynczykszymon@gmail.com>
2022-01-28 11:41:52 +0100

    adding saving to array

--- index.py
+++ index.py
@@ -1,6 +1,10 @@
 from csv import reader
 data = []
+count=0
 with open('file.csv', 'r') as read_obj:
     csv_reader = reader(read_obj)
     for row in csv_reader:
         print(row)
+	data[count]=row
+	count+=1
+
